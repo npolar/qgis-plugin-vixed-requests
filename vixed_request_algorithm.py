cfb27bd3f58725f8db03e82822281ba92b099579
Mikhail Itkin <itkin.m@gmail.com>
2021-04-25 23:53:10 +0200

    Delete duplicate methods
    They have been duplicated via clumsy copy paste

--- vixed_request_algorithm.py
+++ vixed_request_algorithm.py
@@ -211,46 +211,46 @@ class VixedTestAlgorithm(QgsProcessingAlgorithm):
         }
         
 
-    def name(self):
-        """
-        Returns the algorithm name, used for identifying the algorithm. This
-        string should be fixed for the algorithm, and must not be localised.
-        The name should be unique within each provider. Names should contain
-        lowercase alphanumeric characters only and no spaces or other
-        formatting characters.
-        """
-        return 'Generate SAR request form'
+    # def name(self):
+    #     """
+    #     Returns the algorithm name, used for identifying the algorithm. This
+    #     string should be fixed for the algorithm, and must not be localised.
+    #     The name should be unique within each provider. Names should contain
+    #     lowercase alphanumeric characters only and no spaces or other
+    #     formatting characters.
+    #     """
+    #     return 'Generate SAR request form'
 
-    def displayName(self):
-        """
-        Returns the translated algorithm name, which should be used for any
-        user-visible display of the algorithm name.
-        """
-        return self.tr(self.name())
+    # def displayName(self):
+    #     """
+    #     Returns the translated algorithm name, which should be used for any
+    #     user-visible display of the algorithm name.
+    #     """
+    #     return self.tr(self.name())
 
-    def group(self):
-        """
-        Returns the name of the group this algorithm belongs to. This string
-        should be localised.
-        """
-        return self.tr(self.groupId())
+    # def group(self):
+    #     """
+    #     Returns the name of the group this algorithm belongs to. This string
+    #     should be localised.
+    #     """
+    #     return self.tr(self.groupId())
 
-    def groupId(self):
-        """
-        Returns the unique ID of the group this algorithm belongs to. This
-        string should be fixed for the algorithm, and must not be localised.
-        The group id should be unique within each provider. Group id should
-        contain lowercase alphanumeric characters only and no spaces or other
-        formatting characters.
-        """
-        return ''
+    # def groupId(self):
+    #     """
+    #     Returns the unique ID of the group this algorithm belongs to. This
+    #     string should be fixed for the algorithm, and must not be localised.
+    #     The group id should be unique within each provider. Group id should
+    #     contain lowercase alphanumeric characters only and no spaces or other
+    #     formatting characters.
+    #     """
+    #     return ''
 
-    def tr(self, string):
-        return QCoreApplication.translate('Processing', string)
+    # def tr(self, string):
+    #     return QCoreApplication.translate('Processing', string)
 
 
-    def createInstance(self):
-        return VixedRequestsAlgorithm()
+    # def createInstance(self):
+    #     return VixedRequestsAlgorithm()
 
 
     def wktPolygonToDict(self, extent):
@@ -329,6 +329,7 @@ class VixedTestAlgorithm(QgsProcessingAlgorithm):
 
     def createInstance(self):
         return VixedTestAlgorithm()
+
 
 class VixedOnlineAlgorithm(VixedTestAlgorithm):
     def name(self):
