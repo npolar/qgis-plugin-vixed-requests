79b3582169f2c3a98d44f4ff67b7cefc79f6cda4
Mikhail Itkin <itkin.m@gmail.com>
2021-04-25 23:59:25 +0200

    formatting

--- vixed_request_algorithm.py
+++ vixed_request_algorithm.py
@@ -9,7 +9,6 @@
                               -------------------
         begin                : 2021-04-24
         copyright            : (C) 2021 by Mikhail Itkin/NPOLAR
-        email                : user@example.com
  ***************************************************************************/
 
 /***************************************************************************
@@ -22,9 +21,9 @@
  ***************************************************************************/
 """
 
-__author__ = 'Mikhail Itkin/NPOLAR'
+__author__ = 'Mikhail Itkin'
 __date__ = '2021-04-24'
-__copyright__ = '(C) 2021 by Mikhail Itkin/NPOLAR'
+__copyright__ = '(C) 2021 by Mikhail Itkin'
 
 # This will get replaced with a git SHA1 when you do a git archive
 
@@ -47,7 +46,7 @@ from qgis.core import (
     QgsProcessingParameterDateTime,
     QgsCoordinateTransform,
     QgsCoordinateReferenceSystem
-    )
+)
 
 
 class VixedTestAlgorithm(QgsProcessingAlgorithm):
@@ -210,49 +209,6 @@ class VixedTestAlgorithm(QgsProcessingAlgorithm):
             "END_DATE": end_date
         }
         
-
-    # def name(self):
-    #     """
-    #     Returns the algorithm name, used for identifying the algorithm. This
-    #     string should be fixed for the algorithm, and must not be localised.
-    #     The name should be unique within each provider. Names should contain
-    #     lowercase alphanumeric characters only and no spaces or other
-    #     formatting characters.
-    #     """
-    #     return 'Generate SAR request form'
-
-    # def displayName(self):
-    #     """
-    #     Returns the translated algorithm name, which should be used for any
-    #     user-visible display of the algorithm name.
-    #     """
-    #     return self.tr(self.name())
-
-    # def group(self):
-    #     """
-    #     Returns the name of the group this algorithm belongs to. This string
-    #     should be localised.
-    #     """
-    #     return self.tr(self.groupId())
-
-    # def groupId(self):
-    #     """
-    #     Returns the unique ID of the group this algorithm belongs to. This
-    #     string should be fixed for the algorithm, and must not be localised.
-    #     The group id should be unique within each provider. Group id should
-    #     contain lowercase alphanumeric characters only and no spaces or other
-    #     formatting characters.
-    #     """
-    #     return ''
-
-    # def tr(self, string):
-    #     return QCoreApplication.translate('Processing', string)
-
-
-    # def createInstance(self):
-    #     return VixedRequestsAlgorithm()
-
-
     def wktPolygonToDict(self, extent):
         
         if self.CRS.upper() != "EPSG:4326":
